90e3a88d9e21813dc0540ab83f1f541c08331e2a
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-10-22 09:38:07 +0200

    Add basic Json de-serialization test

---
+++ Test/JsonTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Test
+{
+    [TestClass]
+    public class JsonTest
+    {
+        [TestMethod]
+        public void JsonDeSerialize()
+        {
+            var original = new TestMe { AString = "a string", ADate = DateTime.Now };
+            original.SubTestMe.AString = "a sub string";
+            original.SubTestMe.ADate = DateTime.Now.AddDays(+1);
+            var json = original.ToJson();
+
+
+            var challenge = new TestMe();
+            challenge.ReadJson(new JsonTextReader(new StringReader(json)));
+
+            Assert.AreEqual(original.AString, challenge.AString);
+            Assert.AreEqual(original.ADate, challenge.ADate);
+            Assert.AreEqual(original.SubTestMe.AString, challenge.SubTestMe.AString);
+            Assert.AreEqual(original.SubTestMe.ADate, challenge.SubTestMe.ADate);
+        }
+    }
+}
